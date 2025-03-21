ddb99014dc1c0a3010cb9efbf62e1b1e6d10ba09
TudorOrban <tudororban2@gmail.com>
2025-03-21 14:38:41 +0200

    feat(main): Continue fleshing out Main components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 import { SidebarComponent } from "./core/main/components/sidebar/sidebar.component";
+import { filter } from "rxjs";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
 export class AppComponent {
-    title = "renai-frontend";
-
+    isDashboardRoute = false;
+  
+    constructor(private router: Router) {
+        this.router.events
+            .pipe(filter((event) => event instanceof NavigationEnd))
+            .subscribe((event: NavigationEnd) => {
+                this.isDashboardRoute = event.url.startsWith("/dashboard");
+            });
+    }
     
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './dashboard/overview/components/overview/overview.component';
+import { HomeComponent } from './core/main/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "dashboard/overview",
+        component: OverviewComponent
+    }
+];
