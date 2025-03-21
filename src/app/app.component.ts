41a23881c6f788d94854764ba95cd911bec541c9
TudorOrban <tudororban2@gmail.com>
2025-03-21 13:29:32 +0200

    feat(main): Add Header and Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { SidebarComponent } from "./core/main/components/sidebar/sidebar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, SidebarComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css"
 })
 export class AppComponent {
-    title = 'renai-frontend';
+    title = "renai-frontend";
+
+    
 }
